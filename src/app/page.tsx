2759399ca19300e71258a27b3258e6e10beefa02
Rohan Chaudhary <rohankumarchaudhary67@gmail.com>
2024-06-27 12:40:46 +0530

    to learn aws

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -52,9 +52,6 @@ export default function Home() {
               -&gt;
             </span>
           </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
         </a>
 
         <a
